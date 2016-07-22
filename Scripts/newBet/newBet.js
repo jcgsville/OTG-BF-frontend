c8bf45f6762bea89a87d5a7b3fd30dccf0b7c4b2
Chandler Gonzales <Chandler.Gonzales@blackbaud.me>
2016-07-22 01:06:32 -0400

    css changes

--- Scripts/newBet/newBet.js
+++ Scripts/newBet/newBet.js
@@ -1,11 +1,27 @@
 (function () {
     "use strict";
 
-    function newBetFactory() {
+    function newBetFactory($http) {
+
+    	function loadCharityOptions() {
+    		// $http({
+    		// 	method: 'POST',
+    		// 	url: 'https://api.backand.com/1/query/data/startSession?parameters=%7B%22email%22:%22jim.mouer@blackbaud.com%22,%22password%22:%22hunter2%22%7D',
+    		// 	headers: {}
+    		// }).then(function successCallback(response) {
+    		// 	var temp = 
+    		// }, function errorCallback(response) {
+
+    		// });
+    		return [
+    		]
+    	}
+
+
 
     }
 
-
+    newBetFactory.$inject = ['$http']
 
     angular.module('betterBets.newBet', [])
         .factory('newBet', newBetFactory);
